581ebad12dc74b7890df063737e214c9572962ca
Jorge A Valdes Camacho <jorgito.avc03@gmail.com>
2026-04-07 13:30:42 -0400

    Update `GetGreetingUseCase` to include compatibility annotations.
    - **Core Domain**:
        - Add `@RequiresApi(Build.VERSION_CODES.O)` to the `invoke` method in `GetGreetingUseCase` to support Java Time APIs.

--- core/domain/src/main/java/com/saico/ada/domain/use_case/GetGreetingUseCase.kt
+++ core/domain/src/main/java/com/saico/ada/domain/use_case/GetGreetingUseCase.kt
@@ -1,11 +1,14 @@
 package com.saico.ada.domain.use_case
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import java.time.LocalTime
 import javax.inject.Inject
 
 enum class GreetingTime { MORNING, AFTERNOON, EVENING }
 
 class GetGreetingUseCase @Inject constructor() {
+    @RequiresApi(Build.VERSION_CODES.O)
     operator fun invoke(): GreetingTime {
         val now = LocalTime.now()
         return when (now.hour) {
